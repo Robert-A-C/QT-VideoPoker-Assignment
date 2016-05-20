f7a01bfa5fa7f8f15cccb9fd7c21cd6ab11fb5ee
Robert Carll <Robert Carll>
2016-05-20 09:27:08 -0300

    Initial commit for pull

--- Model/videopokergame.h
+++ Model/videopokergame.h
@@ -1,3 +1,14 @@
+///////////////////////////////////////////////////////////////////////
+// File: videopokergame.h
+// Author:
+// This assignment represents my own work and is in accordance with the College Academic Policy
+// Copyright (c) 2015 All Right Reserved by Robert Carll
+// Contributors:
+// Description:
+// Date: May 2016
+// Revisions:
+//////////////////////////////////////////////////////////////////////
+
 #ifndef VIDEOPOKERGAME_H
 #define VIDEOPOKERGAME_H
 
